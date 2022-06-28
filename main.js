f4dc0504500be9a252160033b3dffcc96d49ba74
Jaidenispog <84962743+Jaidenispog@users.noreply.github.com>
2022-06-28 16:55:54 +1000

    Add files via upload

---
+++ main.js
@@ -0,0 +1,68 @@
+var last_pos_x, last_pos_y;
+color="red";
+width_of_line = 2;
+    canvas = document.getElementById('myCanvas');
+    ctx = canvas.getContext("2d");
+
+    var width=screen.width;
+    new_width=screen.width-70;
+    new_height=screen.height-300;
+   
+
+    if (width<992){
+
+        document.getElementById("myCanvas").width=new_width;
+        document.getElementById("myCanvas").height=new_height;
+        document.body.style.overflow="hidden";
+    }
+
+
+
+    canvas.addEventListener("touchstart", my_touchstart);
+    
+    function my_touchstart(e)
+    {
+        
+        color = document.getElementById("color").value;
+        width_of_line = document.getElementById("width_of_line").value;
+        
+        console.log("touch has been detected");
+        last_pos_x=e.touches[0].clientX-canvas.offsetLeft;
+        last_pos_y=e.touches[0].clientY-canvas.offsetTop;
+    }
+
+    canvas.addEventListener("touchmove", my_touchmove);
+    function my_touchmove(e)
+    {
+        current_position_of_touch_x = e.touches[0].clientX - canvas.offsetLeft;
+        current_position_of_touch_y = e.touches[0].clientY - canvas.offsetTop;
+
+  
+        console.log("Current position of x and y coordinates = ");
+        console.log("x  = " + current_position_of_touch_x + "y = " + current_position_of_touch_y);
+        ctx.beginPath();
+        ctx.strokeStyle = color;
+        ctx.lineWidth = width_of_line;
+
+        console.log("Last position of x and y coordinates = ");
+        console.log("x  = " + last_pos_x + "y = " + last_pos_y);
+
+        ctx.moveTo(last_pos_x, last_pos_y);
+
+        console.log("Current position of x and y coordinates = ");
+        console.log("x  = " + current_position_of_touch_x + "y = " + current_position_of_touch_y);
+
+        ctx.lineTo(current_position_of_touch_x, current_position_of_touch_y);
+        
+        ctx.stroke();
+        last_pos_x=current_position_of_touch_x;
+        last_pos_y=current_position_of_touch_y;
+
+    }
+
+   
+
+
+function clearArea() {
+    ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+}
